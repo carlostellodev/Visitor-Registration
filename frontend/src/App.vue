2e3f997a5e829157d1caa0f091438dfc7022fc2c
carlostellodev <carlostello.dev@gmail.com>
2025-10-11 20:19:31 +0200

    ..

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,29 +16,29 @@ const theme = useTheme()
 
 //-----------------------------------------------------------------------------
 // Aplicar theme del tenant a Vuetify
-watch(
-  () => authStore.tenant?.theme !== null,
-  (newTheme) => {
-    console.log(authStore.tenant?.theme);
+// watch(
+//   () => authStore.tenant?.theme === null,
+//   (newTheme) => {
+//     console.log(authStore.tenant?.theme);
 
-    if (newTheme && authStore.tenant) {
-      theme.themes.value.light.colors.primary = newTheme.primary || '#2986cc';
-      theme.themes.value.light.colors.secondary = newTheme.secondary || '#9fc5e8';
-    }
-  },
-  { immediate: true }
-);
+//     if (newTheme && authStore.tenant) {
+//       theme.themes.value.light.colors.primary = newTheme.primary || '#2986cc';
+//       theme.themes.value.light.colors.secondary = newTheme.secondary || '#9fc5e8';
+//     }
+//   },
+//   { immediate: true }
+// );
 //-----------------------------------------------------------------------------
 
 //-----------------------------------------------------------------------------
 // Computed para el gradiente de fondo dinámico
-// const backgroundGradient = computed(() => {
-//   console.log(authStore.tenant?.theme);
+const backgroundGradient = computed(() => {
+  console.log(authStore.tenant?.theme);
 
-//   const primary = authStore.tenant?.theme?.primary || '#667eea'
-//   const secondary = authStore.tenant?.theme?.secondary || '#9fc5e8'
-//   // return `linear-gradient(135deg, ${primary} 0%, ${secondary} 100%)`
-//   return `${secondary}`;
-// })
+  const primary = authStore.tenant?.theme?.primary || '#667eea'
+  const secondary = authStore.tenant?.theme?.secondary || '#9fc5e8'
+  // return `linear-gradient(135deg, ${primary} 0%, ${secondary} 100%)`
+  return `${secondary}`;
+})
 //-----------------------------------------------------------------------------
 </script>
